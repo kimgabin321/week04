448da0823f5dd547e0d3fec9bfc63b8d42ff549d
kimgabin321 <147697405+kimgabin321@users.noreply.github.com>
2023-11-05 01:47:39 +0900

    Add files via upload

---
+++ Components/Movie/Movie.style.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Body = styled.body`
+  background-color: rgb(7, 3, 94);
+`;
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const MovieContainer = styled.div`
+  width: 250px;
+  margin: 15px;
+  background-color: rgb(11, 5, 123);
+  color: white;
+`;
+
+export const MovieImage = styled.img`
+  width: 100%;
+`;
+
+export const MovieInfo = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+  align-items: center;
+`;
